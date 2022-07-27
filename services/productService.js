556c14b3dee4b60036420706fbfb1e3f2c949784
Enrique <octepc.97@gmail.com>
2022-07-27 15:16:41 -0500

    added sequelize to the proyect

--- services/productService.js
+++ services/productService.js
@@ -1,14 +1,12 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('./../libs/postgresPool');
+const sequelize = require('./../libs/sequelize');
 
 class ProductService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.log(err));
   }
   async generate() {
     const limit = 100;
@@ -35,8 +33,10 @@ class ProductService {
 
   async find() {
     const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data, metadata] = await sequelize.query(query);
+    return {
+      data,
+    };
   }
 
   async findOne(id) {
